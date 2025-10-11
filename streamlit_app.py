017171ed821b87cf02601cd7a990d5a332b1a847
Ayush Shukla <asdharupur1@gmail.com>
2025-10-11 12:08:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -725,4 +725,5 @@ st.markdown("""
         <a href="https://www.linkedin.com/in/ayush-shukla-890072337/" target="_blank" style="text-decoration: none;">
             <img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn">
         </a>
-        <a href="
+
+        <a href=""")
